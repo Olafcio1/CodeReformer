38db181ea69966288ca157ad9654dd8ad1e190b9
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-03-07 22:58:14 +0100

    Ready NewProjectScreen

--- reformer/app/home/new_project/NewProjectScreen.py
+++ reformer/app/home/new_project/NewProjectScreen.py
@@ -8,10 +8,20 @@ from ....lib.screen.Screen import Screen
 from ....lib.screen.widget.Container import Container
 from ....lib.screen.widget.ClippedWidget import ClippedWidget
 
+from ....util.settings.Setting import Setting
+from ....util.settings.set_value.StringValue import StringValue
+
+from ...settings import widget as SeWidget
+
 from .Generator import Generator
 from .GeneratorsEvent import GeneratorsEvent
 
+from typing import Callable
+
 class NewProjectScreen(Screen):
+    gensEl: Container
+    formEl: Container
+
     def init(self) -> None:
         window = Container()
         window.style.display("grid")
@@ -24,8 +34,11 @@ class NewProjectScreen(Screen):
         content = Container(0, 30, self.width, self.height - 30)
         content.style.display("flex")
 
-        content.addREWidget(self.makeGeneratorsContainer())
-        content.addREWidget(self.makeFormContainer())
+        content.addREWidget(gensEl := self.__makeGeneratorsContainer())
+        content.addREWidget(formEl := self.__makeFormContainer())
+
+        self.gensEl = gensEl
+        self.formEl = formEl
 
         window.addREWidget(titlebar)
         window.addREWidget(content)
@@ -36,32 +49,83 @@ class NewProjectScreen(Screen):
         name: str
         icon: pygame.Surface
 
+        onClick: Callable[[], None]
+
         def __init__(
                 self,
                 x: int, y: int,
                 width: int, height: int,
                 *,
-                name: str,
-                icon: pygame.Surface
+                generator: Generator,
+                nps: "NewProjectScreen"
         ):
             super().__init__(x, y, width, height)
 
-            self.name = name
-            self.icon = pygame.transform.smoothscale(icon, (min(25, self.height - 5), min(25, self.height - 5)))
+            self.name = generator.getName()
+            self.icon = pygame.transform.smoothscale(generator.getIcon(), (min(25, self.height - 5), min(25, self.height - 5)))
+
+            self.onClick = lambda: nps.setGenerator(generator)
 
         def renderClipped(self, surface: pygame.Surface) -> None:
             font = ResourceManager.font['/font/LEXEND.TTF']
             size = font.size(self.name)
 
             if self.isHovered():
-                surface.fill(0xffffff22)
+                surface.fill(0x393a39)
 
             surface.blit(self.icon, (15, (self.height - self.icon.get_height())/2))
             surface.blit(font.render(self.name, True, 0x5f5f5fff), (40, (self.height - size[1])/2))
 
-    def makeGeneratorsContainer(self) -> Container:
+        def mousePressed(self, x: int, y: int, button: int) -> None:
+            if self.isHovered():
+                super().mousePressed(x, y, button)
+                self.onClick()
+
+    class ContentTitle(ClippedWidget):
+        text: str
+
+        def __init__(
+                self,
+                x: int, y: int,
+                width: int, height: int,
+                *,
+                text: str
+        ):
+            super().__init__(x, y, width, height)
+            self.text = text
+
+        def renderClipped(self, surface: pygame.Surface) -> None:
+            font = ResourceManager.font['/font/LEXEND.TTF']
+            font.set_bold(True)
+
+            surface.blit(pygame.transform.smoothscale_by(
+                font.render(self.text.upper(), True, 0xacacacff),
+                .8
+            ), (0, 0))
+
+            font.set_bold(False)
+
+    def setGenerator(self, generator: Generator) -> None:
+        self.formEl.clear()
+        self.formEl.addREWidget(NewProjectScreen.ContentTitle \
+                                                .Builder() \
+                                                    .kw(text=generator.getName())
+                                                    .size(self.formEl.innerWidth, 20)
+                                                .build())
+
+        self.formEl.addREWidget(SeWidget.TextInput \
+                                        .Builder()
+                                            .kw(setting=Setting(
+                                                name="Path",
+                                                value=StringValue("~/CodeReformerProjects")
+                                            ), valueX=ResourceManager.font['/font/LEXEND.TTF'].size("Path")[0])
+                                            .size(350, 20)
+                                        .build())
+
+    def __makeGeneratorsContainer(self) -> Container:
         el = Container(0, 0, 150, self.height - 30)
         el.style.display("grid")
+        el.style.borderRight(0x222322)
 
         event = GeneratorsEvent()
         generators: list[Generator] \
@@ -71,13 +135,22 @@ class NewProjectScreen(Screen):
         EventManager.fire(event)
 
         for gen in generators:
-            el.addREWidget(NewProjectScreen.GeneratorButton.Builder()
-                                                                .kw(name=gen.getName(), icon=gen.getIcon())
-                                                                .size(100, 24)
-                                                            .build())
+            el.addREWidget(NewProjectScreen.GeneratorButton \
+                                           .Builder()
+                                               .kw(generator=gen, nps=self)
+                                               .size(el.width, 34)
+                                           .build()
+                                           .style("""
+                                                  border_bottom: 0x222322
+                                                  """))
 
         return el
 
-    def makeFormContainer(self) -> Container:
+    def __makeFormContainer(self) -> Container:
         el = Container(0, 0, self.width - 150, self.height - 30)
+        el.style.display("grid")
+        el.style.paddingTop(8)
+        el.style.paddingLeft(8)
+        el.style.gap(8)
+
         return el
