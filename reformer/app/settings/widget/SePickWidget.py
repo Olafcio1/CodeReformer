4673a2660957fb215c67e7873819d840931e93b6
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-14 21:19:18 +0200

    [SePickWidget] Look improvements, mouse handling

--- reformer/app/settings/widget/SePickWidget.py
+++ reformer/app/settings/widget/SePickWidget.py
@@ -25,7 +25,7 @@ class SePickWidget(SettingWidget[PickValue]):
         block_padding: Final = 5
         xpadding:      Final = 2
         ypadding:      Final = 3
-        gap:           Final = 2
+        gap:           Final = 8
 
         x = self.x + self.valueX - 5
         w = sum([TextUtil.size(opt, self._font)[0] + \
@@ -51,8 +51,10 @@ class SePickWidget(SettingWidget[PickValue]):
             text = str(opt)
             size = TextUtil.size(text, self._font)
 
-            if (i+1)%2 == 0:
+            if self.value.value == opt:
                 FigureUtil.roundedRect_fill(surface, 0x3c3c3c, (x, y, block_padding*2 + size[0], h))
+            elif (i+1)%2 == 0:
+                FigureUtil.roundedRect_stroke(surface, 0x3c3c3c, (x, y, block_padding*2 + size[0], h))
 
             size = TextUtil.size(text, self._font)
             image = self._font.render(text, True, 0xa2a2a2ff)
@@ -60,11 +62,33 @@ class SePickWidget(SettingWidget[PickValue]):
             surface.blit(image, (x + (block_padding), y + (h - size[1])/2))
 
             x += size[0]
-            x += gap
+
+            if i != len(options) - 1:
+                x += gap
 
     ###########
     ## MOUSE ##
     ###########
 
-    def mouseIn(self, x: int, y: int) -> None:
-        ...
+    def mouseIn(self, mouseX: int, mouseY: int) -> None:
+        options = self.value.options
+
+        block_padding: Final = 5
+        xpadding:      Final = 2
+        ypadding:      Final = 3
+        gap:           Final = 8
+
+        x = 3
+
+        for i, opt in enumerate(options):
+            x += block_padding
+
+            text = str(opt)
+            size = TextUtil.size(text, self._font)
+
+            if mouseX >= x and mouseX <= x + block_padding*2 + size[0]:
+                self.value.value = opt
+                return
+
+            x += size[0]
+            x += gap
