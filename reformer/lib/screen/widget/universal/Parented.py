fb4700034684f6b1b71f84efd1df2489453d2f38
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-15 01:48:56 +0200

    Parented#wrap

--- reformer/lib/screen/widget/universal/Parented.py
+++ reformer/lib/screen/widget/universal/Parented.py
@@ -22,3 +22,22 @@ class Parented(metaclass=ABCMeta):
 
         if parent is not None:
             parent.removeChild(self)
+
+    def wrap(self, width: int|None = None, height: int|None = None, *, offsetX: int = 0, offsetY: int = 0) -> IContainer:
+        \
+          if width   is None: width  = self.width + offsetX
+        elif offsetX != 0:    raise Exception("'offsetX' cannot be provided along with the 'width' parameter")
+
+        \
+          if height  is None: height = self.height + offsetY
+        elif offsetY != 0:    raise Exception("'offsetY' cannot be provided along with the 'height' parameter")
+
+        from ...widget.Container import Container
+
+        container = Container(width, height)
+
+        self.parent.replaceChild(self, container)
+
+        container.addREWidget(self)
+
+        return container
